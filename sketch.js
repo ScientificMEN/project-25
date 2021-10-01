43ef2d7543de624134d86c1f7e582cd4d36016a1
ScientificMEN <79857028+ScientificMEN@users.noreply.github.com>
2021-10-01 20:14:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -62,6 +62,7 @@ function draw() {
 	
 	imageMode(CENTER);
 
+	image(dustbinImg, 1185, 570, 200,200);
 	//image(paperImg,ball.position.y,ball.position.x,radius,radius);
 	//image(paperImg,ball.position.x,ball.position.y,radius/2,radius/2);
 	//ellipse(ball.position.x,ball.position.y,radius,radius);
@@ -71,7 +72,7 @@ function draw() {
 	
 	//image(1185, 570, 200,200);
 	//rect(1185, 570, 200,200);
-	image(dustbinImg, 1185, 570, 200,200);
+	
 	//ellipse(1185, 570, 200,200)
 
 
